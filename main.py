28e4fe80d227cbf4ee0fe15456d3deaf0096691c
chiken-ma-sa <75468245+chiken-ma-sa@users.noreply.github.com>
2020-12-08 13:49:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ res = Response()
 #pg.create_table()
 
 # Herokuの変数からトークンなどを取得
-channel_secret = os.environ['cccf85bd55377e830ee2dc1b2063124f']
-channel_access_token = os.environ['xQrJyG3QBgOi7f6MQQe42n504MccCzXG1YFSke/lLztj28pwEXz9dVeVWJgkGnGkpTWhDxvXqjIsHLJikeKJhpwtXDZTulIs+B3fs750SqdQNai6UE3h/8sN60D/ccBOWWR8rA5LU0yDiUeRdbEWLwdB04t89/1O/w1cDnyilFU=']
+channel_secret = 'cccf85bd55377e830ee2dc1b2063124f'
+channel_access_token ='xQrJyG3QBgOi7f6MQQe42n504MccCzXG1YFSke/lLztj28pwEXz9dVeVWJgkGnGkpTWhDxvXqjIsHLJikeKJhpwtXDZTulIs+B3fs750SqdQNai6UE3h/8sN60D/ccBOWWR8rA5LU0yDiUeRdbEWLwdB04t89/1O/w1cDnyilFU='
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
